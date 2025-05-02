61cb838e54745c96cecf0f0d25240b1630bd6e37
Geun Park <gepark@student.ethz.ch>
2025-05-02 11:18:55 +0200

    view tutorial and postgres_db update

--- scripts/postgresql_db.sql
+++ scripts/postgresql_db.sql
@@ -22,82 +22,90 @@ VALUES
 
 
 -- For View Tutorial 
-CREATE TABLE agents(	
-    AGENT_CODE char(6) NOT NULL PRIMARY KEY, 
-    AGENT_NAME char(40), 
+
+DROP TABLE IF EXISTS sales_agent;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS marketers;
+DROP TABLE IF EXISTS agents;
+DROP TABLE IF EXISTS contractors;
+
+CREATE TABLE sales_agent(	
+    EMPLOYEE_ID char(6) NOT NULL PRIMARY KEY, 
+    EMPLOYEE_NAME char(40), 
     WORKING_AREA char(35), 
     COMMISSION numeric(10,2), 
-    PHONE_NO char(15), 
-    COUNTRY varchar(25) 
+    PHONE_NO char(15)
 );
 
-INSERT INTO agents VALUES ('A007', 'Ramasundar', 'Bangalore', '0.15', '077-25814763', '');
-INSERT INTO agents VALUES ('A003', 'Alex ', 'London', '0.13', '075-12458969', '');
-INSERT INTO agents VALUES ('A008', 'Alford', 'New York', '0.12', '044-25874365', '');
-INSERT INTO agents VALUES ('A011', 'Ravi Kumar', 'Bangalore', '0.15', '077-45625874', '');
-INSERT INTO agents VALUES ('A010', 'Santakumar', 'Chennai', '0.14', '007-22388644', '');
-INSERT INTO agents VALUES ('A012', 'Lucida', 'San Jose', '0.12', '044-52981425', '');
-INSERT INTO agents VALUES ('A005', 'Anderson', 'Brisbane', '0.13', '045-21447739', '');
-INSERT INTO agents VALUES ('A001', 'Subbarao', 'Bangalore', '0.14', '077-12346674', '');
-INSERT INTO agents VALUES ('A002', 'Mukesh', 'Mumbai', '0.11', '029-12358964', '');
-INSERT INTO agents VALUES ('A006', 'McDen', 'London', '0.15', '078-22255588', '');
-INSERT INTO agents VALUES ('A004', 'Ivan', 'Toronto', '0.15', '008-22544166', '');
-INSERT INTO agents VALUES ('A009', 'Benjamin', 'Hampshair', '0.11', '008-22536178', '');
+
+INSERT INTO sales_agent 
+VALUES 
+    ('A001', 'John', 'Bangalore', '0.14', '077-12346674'),
+    ('A002', 'Mukesh', 'Mumbai', '0.11', '029-12358964'),
+    ('A003', 'Alex ', 'London', '0.13', '075-12458969'),
+    ('A004', 'Ivan', 'Toronto', '0.15', '008-22544166'),
+    ('A005', 'Anderson', 'Brisbane', '0.13', '045-21447739'),
+    ('A006', 'McDen', 'London', '0.15', '078-22255588'),
+    ('A007', 'Ramasundar', 'Bangalore', '0.15', '077-25814763'),
+    ('A008', 'Alford', 'New York', '0.12', '044-25874365'),
+    ('A009', 'Benjamin', 'Hampshair', '0.11', '008-22536178'),
+    ('A010', 'Santakumar', 'Chennai', '0.14', '007-22388644'),
+    ('A011', 'Ravi Kumar', 'Bangalore', '0.15', '077-45625874'),
+    ('A012', 'Lucida', 'San Jose', '0.12', '044-52981425');
 
 
-CREATE TABLE customer(	
-    CUST_CODE varchar(6) NOT NULL PRIMARY KEY, 
+CREATE TABLE customers(	
+    CUST_ID varchar(6) NOT NULL PRIMARY KEY, 
     CUST_NAME varchar(40) NOT NULL, 
     CUST_CITY char(35), 
-    WORKING_AREA varchar(35) NOT NULL, 
-    CUST_COUNTRY varchar(20) NOT NULL, 
-    GRADE integer, 
-    OPENING_AMT numeric(12,2) NOT NULL, 
-    RECEIVE_AMT numeric(12,2) NOT NULL, 
     PAYMENT_AMT numeric(12,2) NOT NULL, 
-    OUTSTANDING_AMT numeric(12,2) NOT NULL, 
-    PHONE_NO varchar(17) NOT NULL, 
-    AGENT_CODE char(6) NOT NULL REFERENCES agents
+    EMPLOYEE_ID char(6) NOT NULL,
+    FOREIGN KEY (EMPLOYEE_ID)
+        REFERENCES sales_agent
+        ON DELETE CASCADE
 ); 
 
-INSERT INTO customer VALUES ('C00013', 'Holmes', 'London', 'London', 'UK', '2', '6000.00', '5000.00', '7000.00', '4000.00', 'BBBBBBB', 'A003');
-INSERT INTO customer VALUES ('C00001', 'Micheal', 'New York', 'New York', 'USA', '2', '3000.00', '5000.00', '2000.00', '6000.00', 'CCCCCCC', 'A008');
-INSERT INTO customer VALUES ('C00020', 'Albert', 'New York', 'New York', 'USA', '3', '5000.00', '7000.00', '6000.00', '6000.00', 'BBBBSBB', 'A008');
-INSERT INTO customer VALUES ('C00025', 'Ravindran', 'Bangalore', 'Bangalore', 'India', '2', '5000.00', '7000.00', '4000.00', '8000.00', 'AVAVAVA', 'A011');
-INSERT INTO customer VALUES ('C00024', 'Cook', 'London', 'London', 'UK', '2', '4000.00', '9000.00', '7000.00', '6000.00', 'FSDDSDF', 'A006');
-INSERT INTO customer VALUES ('C00015', 'Stuart', 'London', 'London', 'UK', '1', '6000.00', '8000.00', '3000.00', '11000.00', 'GFSGERS', 'A003');
-INSERT INTO customer VALUES ('C00002', 'Bolt', 'New York', 'New York', 'USA', '3', '5000.00', '7000.00', '9000.00', '3000.00', 'DDNRDRH', 'A008');
-INSERT INTO customer VALUES ('C00018', 'Fleming', 'Brisbane', 'Brisbane', 'Australia', '2', '7000.00', '7000.00', '9000.00', '5000.00', 'NHBGVFC', 'A005');
-INSERT INTO customer VALUES ('C00021', 'Jacks', 'Brisbane', 'Brisbane', 'Australia', '1', '7000.00', '7000.00', '7000.00', '7000.00', 'WERTGDF', 'A005');
-INSERT INTO customer VALUES ('C00019', 'Yearannaidu', 'Chennai', 'Chennai', 'India', '1', '8000.00', '7000.00', '7000.00', '8000.00', 'ZZZZBFV', 'A010');
-INSERT INTO customer VALUES ('C00005', 'Sasikant', 'Mumbai', 'Mumbai', 'India', '1', '7000.00', '11000.00', '7000.00', '11000.00', '147-25896312', 'A002');
-INSERT INTO customer VALUES ('C00007', 'Ramanathan', 'Chennai', 'Chennai', 'India', '1', '7000.00', '11000.00', '9000.00', '9000.00', 'GHRDWSD', 'A010');
-INSERT INTO customer VALUES ('C00022', 'Avinash', 'Mumbai', 'Mumbai', 'India', '2', '7000.00', '11000.00', '9000.00', '9000.00', '113-12345678','A002');
-INSERT INTO customer VALUES ('C00004', 'Winston', 'Brisbane', 'Brisbane', 'Australia', '1', '5000.00', '8000.00', '7000.00', '6000.00', 'AAAAAAA', 'A005');
-INSERT INTO customer VALUES ('C00023', 'Karl', 'London', 'London', 'UK', '0', '4000.00', '6000.00', '7000.00', '3000.00', 'AAAABAA', 'A006');
-INSERT INTO customer VALUES ('C00006', 'Shilton', 'Toronto', 'Toronto', 'Canada', '1', '10000.00', '7000.00', '6000.00', '11000.00', 'DDDDDDD', 'A004');
-INSERT INTO customer VALUES ('C00010', 'Charles', 'Hampshair', 'Hampshair', 'UK', '3', '6000.00', '4000.00', '5000.00', '5000.00', 'MMMMMMM', 'A009');
-INSERT INTO customer VALUES ('C00017', 'Srinivas', 'Bangalore', 'Bangalore', 'India', '2', '8000.00', '4000.00', '3000.00', '9000.00', 'AAAAAAB', 'A007');
-INSERT INTO customer VALUES ('C00012', 'Steven', 'San Jose', 'San Jose', 'USA', '1', '5000.00', '7000.00', '9000.00', '3000.00', 'KRFYGJK', 'A012');
-INSERT INTO customer VALUES ('C00008', 'Karolina', 'Toronto', 'Toronto', 'Canada', '1', '7000.00', '7000.00', '9000.00', '5000.00', 'HJKORED', 'A004');
-INSERT INTO customer VALUES ('C00003', 'Martin', 'Toronto', 'Toronto', 'Canada', '2', '8000.00', '7000.00', '7000.00', '8000.00', 'MJYURFD', 'A004');
-INSERT INTO customer VALUES ('C00009', 'Ramesh', 'Mumbai', 'Mumbai', 'India', '3', '8000.00', '7000.00', '3000.00', '12000.00', 'Phone No', 'A002');
-INSERT INTO customer VALUES ('C00014', 'Rangarappa', 'Bangalore', 'Bangalore', 'India', '2', '8000.00', '11000.00', '7000.00', '12000.00', 'AAAATGF', 'A001');
-INSERT INTO customer VALUES ('C00016', 'Venkatpati', 'Bangalore', 'Bangalore', 'India', '2', '8000.00', '11000.00', '7000.00', '12000.00', 'JRTVFDD', 'A007');
-INSERT INTO customer VALUES ('C00011', 'Sundariya', 'Chennai', 'Chennai', 'India', '3', '7000.00', '11000.00', '7000.00', '11000.00', 'PPHGRTS', 'A010');
+INSERT INTO customers 
+VALUES 
+    ('C00013', 'Holmes', 'London', '6000.00', 'A003'),
+    ('C00001', 'Micheal', 'New York', '3000.00', 'A008'),
+    ('C00020', 'Albert', 'New York', '5000.00', 'A008'),
+    ('C00025', 'Ravindran', 'Bangalore', '5000.00', 'A011'),
+    ('C00024', 'Cook', 'London', '4000.00', 'A006'),
+    ('C00015', 'Stuart', 'London', '6000.00', 'A003'),
+    ('C00002', 'Bolt', 'New York', '5000.00', 'A008'),
+    ('C00018', 'Fleming', 'Brisbane', '7000.00', 'A005'),
+    ('C00021', 'Jacks', 'Brisbane', '7000.00', 'A005'),
+    ('C00019', 'Yearannaidu', 'Bangalore', '8000.00', 'A010'),
+    ('C00005', 'Sasikant', 'Dehli','7000.00', 'A002'),
+    ('C00007', 'Ramanathan', 'Dehli','7000.00', 'A010'),
+    ('C00022', 'Avinash', 'Mumbai', '7000.00', 'A002'),
+    ('C00004', 'Winston', 'Brisbane', '5000.00', 'A005'),
+    ('C00023', 'Karl', 'London', '4000.00', 'A006'),
+    ('C00006', 'Shilton', 'Toronto', '10000.00', 'A004'),
+    ('C00010', 'Charles', 'Hampshair', '6000.00', 'A009'),
+    ('C00017', 'Srinivas', 'Bangalore', '8000.00', 'A007'),
+    ('C00012', 'Steven', 'San Jose', '5000.00', 'A012'),
+    ('C00008', 'Karolina', 'Toronto', '7000.00', 'A004'),
+    ('C00003', 'Martin', 'Toronto', '8000.00', 'A004'),
+    ('C00009', 'Ramesh', 'Mumbai', '8000.00', 'A002'),
+    ('C00014', 'Rangarappa', 'Bangalore', '8000.00', 'A001'),
+    ('C00016', 'Venkatpati', 'Bangalore', '8000.00', 'A007'),
+    ('C00011', 'Sundariya', 'Chennai', '7000.00', 'A010');
 
-CREATE TABLE contractors(	
-    CONTRACTOR_CODE char(6) NOT NULL PRIMARY KEY, 
-    CONTRACTOR_NAME char(40), 
+CREATE TABLE marketers(	
+    EMPLOYEE_ID char(6) NOT NULL PRIMARY KEY, 
+    EMPLOYEE_NAME char(40), 
     WORKING_AREA char(35), 
-    COMMISSION numeric(10,2), 
-    PHONE_NO char(15), 
-    COUNTRY varchar(25) 
+    SALARY numeric(10,2), 
+    PHONE_NO char(15)
 );
 
-INSERT INTO contractors VALUES ('C001', 'John', 'New York', '0.17', '077-11124769', '');
-INSERT INTO contractors VALUES ('C002', 'Sarah ', 'San Francisco', '0.18', '032-89790125', '');
-INSERT INTO contractors VALUES ('C003', 'Liam', 'Dublin', '0.14', '011-33447076', '');
-INSERT INTO contractors VALUES ('C004', 'Richard', 'Vancouver', '0.15', '008-44720826', '');
-INSERT INTO contractors VALUES ('C005', 'Whitaker', 'New York', '0.18', '012-58859201', '');
-INSERT INTO contractors VALUES ('C006', 'Lucia', 'San Jose', '0.15', '032-81027792', '');
+INSERT INTO marketers 
+VALUES 
+    ('A013', 'John', 'New York', '50000', '077-11124769'),
+    ('A014', 'Sarah ', 'San Francisco', '52000', '032-89790125'),
+    ('A015', 'Liam', 'Dublin', '38000', '011-33447076'),
+    ('A016', 'Richard', 'Vancouver', '47000', '008-44720826'),
+    ('A017', 'Whitaker', 'New York', '54000', '012-58859201'),
+    ('A018', 'Lucia', 'San Jose', '49000', '032-81027792');
